3cfcf9fc772a6f06f5cf41b86db1c3c1e7533e66
kaushikbhat <kaushikbhat115@gmail.com>
2025-07-30 09:33:32 +0530

    Program to remove trailing blanks and tabs from each line of input, and to delete entirely blank lines.

--- ChapterOne/Excercises/ex_1.18.c
+++ ChapterOne/Excercises/ex_1.18.c
@@ -2,21 +2,39 @@
 
 #define MAXLINE 1000
 
-int getLine(char line[], char whitespace[], int maxline);
+int getLine(char line[], int maxline);
 
 int main(void) {
-    char line[MAXLINE] = {0}, whitespace[MAXLINE] = {0};
+    char line[MAXLINE] = {0};
     int len;
 
     while((len = getLine(line, MAXLINE))) {
-
+        if(len > 0) {
+            printf("%s\n", line);
+        }
     }
 }
 
-int getLine(char line[], char whitespace[], int maxline) {
+int getLine(char line[], int maxline) {
     int i, c;
+    int lastNonWhitespaceIdx = -1;
 
     for(i = 0; i < maxline - 1 && ((c = getchar()) != EOF) && c != '\n'; i++) {
-        if()
+        if(c != ' ' && c != '\t') 
+            lastNonWhitespaceIdx = i;
+        line[i] = c;
+    }
+
+    if(c == '\n') {
+        if(lastNonWhitespaceIdx >= 0)
+            line[++lastNonWhitespaceIdx] = c;
+    }
+
+    if(lastNonWhitespaceIdx >= 0) {
+        line[++lastNonWhitespaceIdx] = '\0';
+        return lastNonWhitespaceIdx;
+    } else {
+        line[0] = '\0';
+        return 0;
     }
 }
