b4c72ffde44cc1cf02472ae370e5948b1e8a5228
kaushikbhat <kaushikbhat115@gmail.com>
2025-08-03 18:27:28 +0530

    Example program to determine a year is a leap year.

---
+++ ChapterTwo/Example/leapYear.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int year;
+
+    while(scanf("%d", &year) != EOF) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            printf("%d is a leap year\n", year);
+        else
+            printf("%d is not a leap year\n", year);
+    }
+
+}
