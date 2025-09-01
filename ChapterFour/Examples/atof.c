890f5363205982272d79a2476e92da75692e9e97
kaushikbhat <kaushikbhat115@gmail.com>
2025-09-01 08:16:34 +0530

    Program to convert  a string equivalent of a decimal number to its float representaion.

--- ChapterFour/Examples/atof.c
+++ ChapterFour/Examples/atof.c
@@ -1,6 +1,38 @@
 #include <ctype.h>
 #include <stdio.h>
 
+#define MAXLINE 100
+
+double asciiToFloat(char s[]);
+int getLine(char line[], int maxline);
+
+int main(void)
+{
+    char line[MAXLINE] = {0};
+    double sum;
+
+    sum = 0;
+    while ((getLine(line, MAXLINE)) > 0)
+    {
+        printf("\t%g\n", sum += asciiToFloat(line));
+    }
+}
+
+int getLine(char line[], int maxline)
+{
+    int c, i;
+
+    for (i = 0; (c = getchar()) != EOF && c != '\n'; i++)
+        line[i] = c;
+
+    if (c == '\n')
+        line[i++] = c;
+
+    line[i] = '\0';
+
+    return i;
+}
+
 double asciiToFloat(char s[])
 {
     double val, power;
