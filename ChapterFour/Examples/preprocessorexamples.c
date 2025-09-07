a4f7d81c63b80dd0f51f4f5c65783bcc65481d91
kaushikbhat <kaushikbhat115@gmail.com>
2025-09-07 16:38:08 +0530

    Updated preprocessorexamples.c

--- ChapterFour/Examples/preprocessorexamples.c
+++ ChapterFour/Examples/preprocessorexamples.c
@@ -9,13 +9,23 @@
 #define SQUARE(x) ((x) * (x))
 #define CUBE(x) ((x) * (x) * (x))
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define SWAP(t, x, y) \
+    t c = x;          \
+    x = y;            \
+    y = c;
 
 // Generic Macros
 #define DPRINT(expr) printf(#expr " = %d\n", expr);
 #define PASTE(x, y) x##y
 
-int main(void) {
-    DPRINT(ADD(1, 2));
+int main(void)
+{
+//    DPRINT(ADD(1, 2));
+
+    int a = 1, b = 2;
+    SWAP(int, a, b);
+
+    printf("a = %d\tb = %d", a, b);
 
     return 0;
 }
