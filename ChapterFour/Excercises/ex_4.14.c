1877cbb7afee6ea96e08d2765e376d0a9fb24676
kaushikbhat <kaushikbhat115@gmail.com>
2025-09-07 16:40:26 +0530

    Program to swap two numbers using a macro.

---
+++ ChapterFour/Excercises/ex_4.14.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define ADD(x, y) ((x) + (y))
+#define SUB(x, y) ((x) - (y))
+#define MUL(x, y) ((x) * (y))
+#define DIV(x, y) ((y) == 0 ? 0 : (x) / (y))
+#define MOD(x, y) ((x) % (y))
+#define SQUARE(x) ((x) * (x))
+#define CUBE(x) ((x) * (x) * (x))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define SWAP(t, x, y) \
+    t c = x;          \
+    x = y;            \
+    y = c;
+
+// Generic Macros
+#define DPRINT(expr) printf(#expr " = %d\n", expr);
+#define PASTE(x, y) x##y
+
+int main(void)
+{
+    int a = 1, b = 2;
+
+    printf("Before Swapping: a = %d\tb = %d\n", a, b);
+
+    SWAP(int, a, b);
+
+    printf("After Swapping: a = %d\tb = %d\n", a, b);
+
+    return 0;
+}
