bf31acc67d01db17a6ea78260c8a2683322f5077
kaushikbhat <kaushikbhat115@gmail.com>
2025-10-05 21:13:05 +0530

    program on getint using pointers.

--- ChapterFive/Examples/getint.c
+++ ChapterFive/Examples/getint.c
@@ -1,9 +1,9 @@
-#include <ctype.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int static isSpace(char c)
 {
-    return (c == ' ' || c == '\n' || c == '\t');
+    return (c == ' ' || c == '\t' || c == '\n');
 }
 
 int static isDigit(char c)
@@ -12,18 +12,15 @@ int static isDigit(char c)
 }
 
 #define SIZE 10
-
 int buff[SIZE];
 int buffp = 0;
 
 int getch(void);
-void ungetch(int c);
-
-int getint(int *pn);
+void ungetch(int);
 
 int main(void)
 {
-    int n, array[SIZE];
+    int n, array[SIZE], getint(int *);
 
     for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
         ;
@@ -35,43 +32,40 @@ int getint(int *pn)
 {
     int c, sign;
 
-    while (isSpace(c = getch()))
+    while (isSpace(c = getchar()))
         ;
 
-    if (!isDigit(c) && c != EOF && c != '.' && c != '+' && c != '-')
+    if (!isDigit(c) && c != EOF && c != '+' && c != '-')
     {
         ungetch(c);
         return 0;
     }
 
     sign = (c == '-') ? -1 : 1;
+
     if (c == '+' || c == '-')
         c = getch();
 
     for (*pn = 0; isDigit(c); c = getch())
-        ;
-    *pn = *pn * 10 + (c - '0');
-
-    *pn *= sign;
+        *pn = *pn * 10 + (c - '0');
 
     if (c != EOF)
+    {
         ungetch(c);
+    }
 
-    return 0;
+    return c;
 }
 
 int getch(void)
 {
-    return ((buffp > 0) ? buff[--buffp] : getchar());
+    return ((buffp > 0) ? buff[buffp--] : getchr());
 }
 
 void ungetch(int c)
 {
-    if (buffp >= SIZE)
-    {
-        printf("error: ungetch has too many characters!\n");
-        return;
-    }
-
-    buff[buffp++] = c;
+    if (buffp > SIZE - 1)
+        printf("error: too many characters in buff!\n");
+    else
+        buff[buffp++] = c;
 }
